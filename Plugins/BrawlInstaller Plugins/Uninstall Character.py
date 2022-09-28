32a575835fad6a958cae2f07a6eef6e795fba60e
Ryan Theroux <ryantheroux@yahoo.com>
2022-09-27 22:13:17 -0500

    Fixed incorrect error display on other plugins

--- Plugins/BrawlInstaller Plugins/Uninstall Character.py
+++ Plugins/BrawlInstaller Plugins/Uninstall Character.py
@@ -11,7 +11,7 @@ def main():
 				BrawlAPI.ShowMessage("Build path must be set. This can be done by navigating to Tools > Settings > General and setting the 'Default Build Path' to the path to your build's root folder.", "Build Path Not Set")
 				return
 			if not Directory.Exists(MainForm.BuildPath + '/pf/'):
-				BrawlAPI.ShowMessage("Build path does not appear to be valid. Please change your build path by going to 'Tools > Settings' and modifying the 'Default Build Path' field.\n\nYour build path should contain a folder named 'pf' within it.")
+				BrawlAPI.ShowMessage("Build path does not appear to be valid. Please change your build path by going to 'Tools > Settings' and modifying the 'Default Build Path' field.\n\nYour build path should contain a folder named 'pf' within it.", "Invalid Build Path")
 				return
 			createLogFile()
 			backupCheck()
