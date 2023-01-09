17787bd95e6a6b8bf7ea7e867ecb6afe2ff72921
Ryan Theroux <ryantheroux@yahoo.com>
2023-01-08 18:22:53 -0600

    Get patch node path when getting nodes, create empty file for nodes to be removed

--- Plugins/BrawlInstaller Plugins/Create Patch.py
+++ Plugins/BrawlInstaller Plugins/Create Patch.py
@@ -6,9 +6,10 @@ TEMP_PATH = AppPath + '/temp'
 CONTAINERS = [ "BrawlLib.SSBB.ResourceNodes.ARCNode", "BrawlLib.SSBB.ResourceNodes.BRRESNode", "BrawlLib.SSBB.ResourceNodes.BRESGroupNode"]
 
 class NodeObject:
-		def __init__(self, node, md5):
+		def __init__(self, node, md5, patchNodePath):
 			self.node = node
 			self.md5 = md5
+			self.patchNodePath = patchNodePath
 
 # Get all nodes with a particular path
 def findChildren(node, path):
@@ -75,7 +76,17 @@ def getNodeObjects(filePath, closeFile=True):
 			#fileNodes = BrawlAPI.RootNode.GetChildrenRecursive()
 			fileNodes = getPatchNodes(BrawlAPI.RootNode)
 			for fileNode in fileNodes:
-				fileNodeList.append(NodeObject(fileNode, fileNode.MD5Str()))
+				currentNode = fileNode
+				pathNodeNames = []
+				while currentNode.Parent:
+					pathNodeNames.insert(0, getPatchNodeName(currentNode, folder=True))
+					currentNode = currentNode.Parent
+				nodePath = ""
+				i = 0
+				while i < len(pathNodeNames):
+					nodePath += pathNodeNames[i] + '\\' if i < len(pathNodeNames) - 1 else ''
+					i += 1
+				fileNodeList.append(NodeObject(fileNode, fileNode.MD5Str(), nodePath))
 		if closeFile:
 			BrawlAPI.ForceCloseFile()
 		writeLog("Finished getting node objects")
@@ -111,32 +122,16 @@ def getPatchNodeName(node, remove=False, folder=False):
 		return nodeName
 
 # Export node for a patch and create directory if it can't be found
-def exportPatchNode(node, remove=False):
-		writeLog("Exporting patch node " + node.TreePathAbsolute)
-		# Old way, just creates directories based on node tree path
-		#nodeSplit = node.TreePathAbsolute.split('/')
-		#nodePath = ""
-		#i = 0
-		#while i < len(nodeSplit):
-		#	nodePath += nodeSplit[i] + '\\' if i < len(nodeSplit) - 1 else ''
-		#	i += 1
-		# New way, export with an index at the beginning, which is useful for importer if multiple children have the same name
-		currentNode = node
-		pathNodeNames = []
-		while currentNode.Parent:
-			pathNodeNames.insert(0, getPatchNodeName(currentNode, folder=True))
-			currentNode = currentNode.Parent
-		nodePath = ""
-		i = 0
-		while i < len(pathNodeNames):
-			nodePath += pathNodeNames[i] + '\\' if i < len(pathNodeNames) - 1 else ''
-			i += 1
-		createDirectory(TEMP_PATH + '\\' + nodePath)
-		node.Export(TEMP_PATH + '\\' + nodePath + '\\' + getPatchNodeName(node, remove))
+def exportPatchNode(nodeObject):
+		writeLog("Exporting patch node " + nodeObject.node.TreePathAbsolute)
+		createDirectory(TEMP_PATH + '\\' + nodeObject.patchNodePath)
+		if nodeObject.node.MD5Str():
+			nodeObject.node.Export(TEMP_PATH + '\\' + nodeObject.patchNodePath + '\\' + getPatchNodeName(nodeObject.node))
+		else:
+			File.CreateText(TEMP_PATH + '\\' + nodeObject.patchNodePath + '\\' + getPatchNodeName(nodeObject.node, True)).Close()
 		writeLog("Exported patch node")
 
 def main():
-		text = ""
 		createLogFile()
 
 		# If temporary directory already exists, delete it to prevent duplicate files
@@ -172,28 +167,19 @@ def main():
 					matchFound = True
 					# If we've found a match, but MD5s do NOT match, this is an altered node and should be exported
 					if alteredFileNode.md5 != cleanFileNode.md5:
-						exportPatchNode(alteredFileNode.node)
-						text += "\n" + alteredFileNode.node.TreePathAbsolute + ": " + alteredFileNode.md5 + " " + cleanFileNode.md5 + "\n"
+						exportPatchNode(alteredFileNode)
 					break
 			# If we never found a match for a node in the altered file, it's a brand new node, and should be exported
 			if not matchFound:
-				exportPatchNode(alteredFile.node)
-				text += "\n" + alteredFileNode.node.TreePathAbsolute + ": " + "NEW\n"
+				exportPatchNode(alteredFileNode)
 			# If we found a match at all, the clean file node should be removed from the list for comparison, to speed up searches and
 			#to prevent false positives when nodes share paths.
 			if removeNode:
 				cleanFileNodes.remove(removeNode)
 		progressBar.Finish()
 		BrawlAPI.ForceCloseFile()
-		writeLog(text)
-		BrawlAPI.ShowMessage(text, "")
-		# TODO: We need a way to actually get the data and paths for removed nodes
-		removeText = ""
 		# Any nodes remaining in the clean file node list are nodes with no matches in the altered file, meaning they should be removed when the patch is installed
 		for removeNode in cleanFileNodes:
-			exportPatchNode(removeNode.node, True)
-			removeText += "\n" + removeNode.node.Name + "\n"
-		writeLog(removeText)
-		BrawlAPI.ShowMessage(removeText, "")
+			exportPatchNode(removeNode)
 
 main()
