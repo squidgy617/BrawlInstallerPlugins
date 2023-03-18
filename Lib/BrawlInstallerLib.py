185952f7f51f39171b86c9dfc97073699ceb04e3
Ryan Theroux <ryantheroux@yahoo.com>
2023-03-18 11:14:37 -0500

    Fixed issues with removing stages

--- Lib/BrawlInstallerLib.py
+++ Lib/BrawlInstallerLib.py
@@ -5499,7 +5499,7 @@ def removeStageId(fullId, path="/Source/Project+/StageFiles.asm"):
 				newLine = ""
 				j = 0
 				while j < len(splitLine):
-					newLine += splitLine[j].strip() + ("," if j != len(splitLine) - 1 else "") + "\t"
+					newLine += splitLine[j].strip() + ("," if len(splitLine) > 1 else "") + "\t"
 					j += 1
 				newLine += "| "
 				j = 0
