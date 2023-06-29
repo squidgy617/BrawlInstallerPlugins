94ade9520818ab9b2e4ec165eeaeadfcf3e6a69d
Ryan Theroux <ryantheroux@yahoo.com>
2023-06-28 19:09:10 -0500

    Made it possible to cancel out of restoring a backup

--- Plugins/BrawlInstaller Plugins/Restore Backup.py
+++ Plugins/BrawlInstaller Plugins/Restore Backup.py
@@ -16,8 +16,9 @@ def main():
 		for backup in backups:
 			backupString = backupString + str(i) + ' : ' + DirectoryInfo(backup).Name + '\n'
 			i += 1
-		BrawlAPI.ShowMessage("You will be prompted to enter an integer corresponding to one of your backups. Please enter a number corresponding to one of the below options: \n" + backupString, "Backups")
-		backupChoice = BrawlAPI.UserIntegerInput("Enter Backup Number", "Backup Number: ", 1, 1, i)
-		restoreBackup(backups[backupChoice - 1])
+		BrawlAPI.ShowMessage("You will be prompted to enter an integer corresponding to one of your backups. Please enter a number corresponding to one of the below options, or enter 0 to cancel: \n" + backupString, "Backups")
+		backupChoice = BrawlAPI.UserIntegerInput("Enter Backup Number", "Backup Number: ", 0, 0, i)
+		if backupChoice > 0:
+			restoreBackup(backups[backupChoice - 1])
 
 main()
