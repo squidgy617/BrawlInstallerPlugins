cde9319bb0233633c1be518e1a0e6056f9d4aaa8
Ryan Theroux <ryantheroux@yahoo.com>
2023-07-30 18:46:52 -0500

    Implemented adding nodes

--- Plugins/BrawlInstaller Plugins/Apply Patch.py
+++ Plugins/BrawlInstaller Plugins/Apply Patch.py
@@ -7,21 +7,32 @@ def processPatchFiles(patchFolder, node):
 	for patchFile in Directory.GetFiles(patchFolder):
 		writeLog("Processing patch file " + patchFile)
 		patchNode = PatchNode(getFileInfo(patchFile).Name)
-		foundNode = findNodeToPatch(node, patchNode)
-		if foundNode:
-			if patchNode.action == "REPLACE":
-				writeLog("Replacing " + foundNode.Name)
-				foundNode.Replace(patchFile)
-			if patchNode.action == "REMOVE":
-				writeLog("Removing " + foundNode.Name)
-				foundNode.Remove()
-			if patchNode.action == "PARAM":
-				writeLog("Updating params for " + foundNode.Name)
-				tempNode = createNodeFromString(patchNode.typeString)
-				node.AddChild(tempNode)
-				tempNode.Replace(patchFile)
-				copyNodeProperties(tempNode, foundNode)
-				tempNode.Remove()
+		if patchNode.action in [ "REPLACE", "REMOVE", "PARAM" ]:
+			foundNode = findNodeToPatch(node, patchNode)
+			if foundNode:
+				if patchNode.action == "REPLACE":
+					writeLog("Replacing " + foundNode.Name)
+					foundNode.Replace(patchFile)
+				if patchNode.action == "REMOVE":
+					writeLog("Removing " + foundNode.Name)
+					foundNode.Remove()
+				if patchNode.action == "PARAM":
+					writeLog("Updating params for " + foundNode.Name)
+					tempNode = createNodeFromString(patchNode.typeString)
+					node.AddChild(tempNode)
+					tempNode.Replace(patchFile)
+					copyNodeProperties(tempNode, foundNode)
+					tempNode.Remove()
+			elif patchNode.action == "REPLACE":
+				writeLog("Adding " + patchNode.name)
+				newNode = createNodeFromString(patchNode.typeString)
+				node.AddChild(newNode)
+				newNode.Replace(patchFile)
+		elif patchNode.action == "ADD":
+			writeLog("Adding " + patchNode.name)
+			newNode = createNodeFromString(patchNode.typeString)
+			node.AddChild(newNode)
+			newNode.Replace(patchFile)
 	for directory in Directory.GetDirectories(patchFolder):
 		patchNode = PatchNode(DirectoryInfo(directory).Name)
 		newNode = findNodeToPatch(node, patchNode)
